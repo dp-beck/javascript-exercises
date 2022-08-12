d34e47475318644e7be3807cf9d85e62d47a74f0
Dan Beck <beckdp01@gmail.com>
2022-08-11 22:07:51 -0400

    solve 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+//1.) Turn string into array.
+//2.) Reverse Array order.
+//3.) Turn array into string
 
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+//1.) Loop through optional arguments.
+//2.) Find out if optional argument is in array.
+//3.) If optional argument is in provided array, remove it from array -- splice 
+//4.) If not, go to next optional argument.
+//5.) The the function must return the edited array.
 
+const removeFromArray = function() {
+    const args = Array.from(arguments);
+    for (let i=1; i<args.length; i++) {
+        if (args[0].includes(args[i])) {
+            args[0].splice(args[0].indexOf(args[i]), 1);
+        }
+    }
+    return args[0];
 };
 
 // Do not edit below this line
